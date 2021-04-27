8d1a82a80fad89f27957d929d3944cca197ad940
Dayane Damaceno <dayane.damaceno@hotmail.com>
2021-04-27 15:04:17 -0300

    Estudando testes com mocha e nock

--- src/manipularListas/service.js
+++ src/manipularListas/service.js
@@ -5,7 +5,12 @@ const urlPrefix = 'https://swapi.dev/api/people';
 export default async function obterPessoas(nome){
   const url = `${urlPrefix}/?search=${nome}&format=json`;
   const { data } = await axios.get(url);
-  return data;
+  return data.results.map((item) => {
+    return {
+      nome: item.name,
+      peso: item.height
+    }
+  });
 }
 
 // export {
